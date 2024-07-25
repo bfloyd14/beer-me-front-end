abb2b5d732aa5dec4fcf03d61b2e5c90d1cd5758
bfloyd14 <brendanflo1425@gmail.com>
2024-07-25 12:10:56 -0400

    seperate profile photo from li on navbar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,13 +15,17 @@ const NavBar = ({ user, handleLogout, profile }) => {
   )
 
   const protectedLinks = (
-    <ul>
-      <NavLink to='/profiles/:profileId'><img className={styles.image} src={profile.photo} alt="beer-me logo" /></NavLink>
-      <li><NavLink to='/new'>New Beer</NavLink></li>
-      <li><NavLink to='/beers'>Beers</NavLink></li>
-      <li><NavLink to="/profiles">Profiles</NavLink></li>
-      <li><NavLink to="" onClick={handleLogout}>Logout</NavLink></li>
+    <>
+    <ul className={styles.profile}>
+        <NavLink to='/profiles/:profileId'><img className={styles.image} src={profile.photo} alt="beer-me logo" /></NavLink>
     </ul>
+    <ul className={styles.navBar}>
+        <li><NavLink to='/new'>New Beer</NavLink></li>
+        <li><NavLink to='/beers'>Beers</NavLink></li>
+        <li><NavLink to="/profiles">Profiles</NavLink></li>
+        <li><NavLink to="" onClick={handleLogout}>Logout</NavLink></li>
+    </ul>
+    </>
   )
 
   return (
